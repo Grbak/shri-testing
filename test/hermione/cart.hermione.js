11fed3a6d1d484f7251ec0896ded68f2567421bc
Vladislav Dybok <eden91hazard@gmail.com>
2023-07-02 22:44:11 +0300

    ШРИ-6: Добавил тест, проверяющий отправку формы заказа на сервер

--- test/hermione/cart.hermione.js
+++ test/hermione/cart.hermione.js
@@ -1,44 +1,4 @@
 
-describe('Оформленный заказ', async function() {
-    it('отображается корректно', async function({ browser }) {
-        const puppeteer = await browser.getPuppeteer();
-        const [page] = await puppeteer.pages();
-
-        await page.goto('http://localhost:3000/hw/store');
-
-        const mock = {
-            1: {
-                name: 'test',
-                price: 100,
-                count: 1
-            }
-        }
-
-        await page.evaluate(mock => {
-            window.localStorage.clear();
-            window.localStorage.setItem('example-store-cart', JSON.stringify(mock));
-        }, mock);
-
-        await page.goto('http://localhost:3000/hw/store/cart');
-
-        await page.focus('.Form-Field_type_name');
-        await page.keyboard.type('test');
-        await page.focus('.Form-Field_type_phone');
-        await page.keyboard.type('111-111-1111');
-        await page.focus('.Form-Field_type_address');
-        await page.keyboard.type('test');
-
-        const checkoutButton = await page.waitForSelector('[data-testid="button-submit"]');
-        await checkoutButton.click();
-
-        await browser.assertView('plain', '[data-testid="order_status"]', {
-            ignoreElements: [
-                '[data-testid="paragraph-order_info"]'
-            ]
-        });
-    });
-});
-
 describe('Корзина', () => {
     it('очищается при нажатии на соответствующую кнопку', async ({ browser }) => {
         const puppeteer = await browser.getPuppeteer();
---
+++ test/hermione/order.hermione.js
@@ -0,0 +1,40 @@
+
+describe('Оформленный заказ', async function() {
+    it('отображается корректно', async function({ browser }) {
+        const puppeteer = await browser.getPuppeteer();
+        const [page] = await puppeteer.pages();
+
+        await page.goto('http://localhost:3000/hw/store');
+
+        const mock = {
+            1: {
+                name: 'test',
+                price: 100,
+                count: 1
+            }
+        }
+
+        await page.evaluate(mock => {
+            window.localStorage.clear();
+            window.localStorage.setItem('example-store-cart', JSON.stringify(mock));
+        }, mock);
+
+        await page.goto('http://localhost:3000/hw/store/cart');
+
+        await page.focus('.Form-Field_type_name');
+        await page.keyboard.type('test');
+        await page.focus('.Form-Field_type_phone');
+        await page.keyboard.type('111-111-1111');
+        await page.focus('.Form-Field_type_address');
+        await page.keyboard.type('test');
+
+        const checkoutButton = await page.waitForSelector('[data-testid="button-submit"]');
+        await checkoutButton.click();
+
+        await browser.assertView('plain', '[data-testid="order_status"]', {
+            ignoreElements: [
+                '[data-testid="paragraph-order_info"]'
+            ]
+        });
+    });
+});
