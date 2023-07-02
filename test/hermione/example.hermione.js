4a9c085f0cb57fb10d92661443f3a62d1065f9c3
Vladislav Dybok <eden91hazard@gmail.com>
2023-07-02 14:56:20 +0300

    ШРИ-6: Написал свой первый тест с использованием Hermione

--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -1,11 +1,35 @@
-const { assert } = require('chai');
 
-describe('microsoft', async function() {
-    it('Тест, который пройдет', async function() {
-        await this.browser.url('https://www.microsoft.com/ru-ru/');
-        await this.browser.assertView('plain', 'body');
+describe('Карточка товара', async function() {
+    it('отображается корректно', async function({ browser }) {
+        const puppeteer = await browser.getPuppeteer();
+        const [page] = await puppeteer.pages();
 
-        const title = await this.browser.$('#uhfLogo').getText();
-        assert.equal(title, 'Microsoft');
+        const productInfo = {
+            id: 1,
+            name: 'test',
+            price: 100
+        }
+
+        const product = {
+            ...productInfo,
+            description: 'test',
+            color: 'test',
+            material: 'test'
+        }
+
+        const productsMock = await browser.mock('http://localhost:3000/hw/store/api/products');
+        productsMock.respond([
+            productInfo
+        ])
+
+        await page.goto('http://localhost:3000/hw/store/catalog');
+
+        const detailMock = await browser.mock(`http://localhost:3000/hw/store/api/products/${productInfo.id}`);
+        detailMock.respond(product)
+
+        const element = await page.waitForSelector('[data-testid="link-product_detail"');
+        await element.click();
+
+        await browser.assertView('plain', 'body');
     });
 });
